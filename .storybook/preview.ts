c0135e7807dd3f5cf4113b910cef486bcc9fde6c
Damir Bogdanov <89067770+damirbogdanov@users.noreply.github.com>
2026-02-18 15:11:52 +1100

    feat: use CSS specific vars

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,9 +3,41 @@ import { defineCustomElements } from "../dist/esm/loader";
 
 defineCustomElements();
 
+const vars = {
+  light: {
+    '--tw-dates-text-color':'#111',
+    '--tw-dates-secondary-color':'#767676',
+    '--tw-dates-border-color':'#949494',
+    '--tw-dates-bg-color':' #fff',
+    '--tw-dates-disabled-color':'#959595',
+    '--tw-dates-hover-color':'#e5e7eb',
+    '--tw-dates-bg2-color':' #f2f3f5',
+    '--tw-dates-focus-color':'#0000ff',
+    '--tw-dates-error-color':'#d4351c',
+    '--tw-dates-active-color':'#0000ff',
+  },
+  dark: {
+    '--tw-dates-text-color':'#fff',
+    '--tw-dates-secondary-color':' #bcbcbcff',
+    '--tw-dates-border-color':' #595959',
+    '--tw-dates-bg-color':' #14171e',
+    '--tw-dates-disabled-color':' #616161',
+    '--tw-dates-hover-color':' #3a3d4a',
+    '--tw-dates-bg2-color':' #2b2d37',
+    '--tw-dates-focus-color':' #ffbf00',
+    '--tw-dates-error-color':' #f16c57',
+    '--tw-dates-active-color':' #ffbf00',
+  },
+}
+
 function applyTheme(ctx: any) {
   const theme = ctx.globals?.backgrounds?.value === "dark" ? "dark" : "light";
   document.documentElement.dataset.theme = theme;
+
+  const themeVars = vars[theme];
+  for (const [varName, varValue] of Object.entries(themeVars)) {
+    document.documentElement.style.setProperty(varName, varValue);
+  }
 }
 
 const preview: Preview = {
